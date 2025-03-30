67360a44c6ee57d9b06999d6499348deca7879a1
Frans Stefanus Bothma <fstefanusbothma@gmail.com>
2025-03-30 22:05:24 +0200

    chore(docker): add containerisation

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,13 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
     let configuration = configuration::get_configuration().expect("Failed to read config");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection =
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let bound_addr = TcpListener::bind(address)?.local_addr()?;
     zero2prod::startup::run(&bound_addr, connection)?.await
 }
--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::async_yields_async)]
 use actix_web::{HttpResponse, Responder};
 
 #[tracing::instrument(name = "Performing heatlth check")]
